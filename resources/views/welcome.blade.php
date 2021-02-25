1147bec2760f3aa303698604a5e91a5502119654
wouterafca1 <w.2002kogelman@gmail.com>
2021-02-25 10:32:20 +0100

    Update welcome.blade.php
    !!!!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>SaMSam Rooster</title>
+        <title>SaMSam Rooster!!!!!!!!!!!!!!!!</title>
 
         <!-- Styles -->
         <style>
