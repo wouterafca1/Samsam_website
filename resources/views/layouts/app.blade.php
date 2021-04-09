7e22511754fa723b596dd6250365bc19e38a0b4f
wouterafca1 <w.2002kogelman@gmail.com>
2021-04-09 11:07:58 +0200

    hekkie
    hekkie

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,16 +61,8 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Account') }}
-                                    </a>
-                                    
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
-
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
