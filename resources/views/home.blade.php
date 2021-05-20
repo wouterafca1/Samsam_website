7f277ad7c7045dcf36fc6ad2d17267128a0be3f8
wouterafca1 <w.2002kogelman@gmail.com>
2021-05-20 11:53:04 +0200

    week + kalender

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,23 @@
 @section('content')
 
 <div class="container">
+    <div id="sidemenu" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+    </div>
+    <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+    <script>
+        function openNav() {
+            document.getElementById("sidemenu").style.width = "250px";
+        }
+
+        function closeNav() {
+            document.getElementById("sidemenu").style.width = "0";
+        }
+    </script>
     <?php
     $dt = new DateTime;
     if (isset($_GET['year']) && isset($_GET['week'])) {
@@ -18,8 +35,9 @@
     <a href="<?php echo $_SERVER['PHP_SELF'].'?week='.($week+1).'&year='.$year; ?>">Next Week</a> <!--Next week-->
 
     <table>
-        <tr>
-            <th>Werknemer: </th>
+        <tr class="list">
+
+            <th class="week">Week: <?php echo $week; ?>  </th>
             <?php
             do {
                 echo "<th class='dag'>" . $dt->format('l') . "<br>" . $dt->format('d M Y') . "</th>\n";
@@ -28,8 +46,15 @@
             ?>
         </tr>
         @foreach($users as $user)
-            <tr class="list">
-                <td>{{ $user->name }}</td>
+            <tr>
+                <td class="naam">{{ $user->name }}</td>
+                <td class="content">1</td>
+                <td class="content">2</td>
+                <td class="content">3</td>
+                <td class="content">4</td>
+                <td class="content">5</td>
+                <td class="content">6</td>
+                <td class="content">7</td>
             </tr>
         @endforeach
     </table>
