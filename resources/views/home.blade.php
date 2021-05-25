7e2a08e70bb0619158b14bcdfac97e9644be53a1
ThatsJoey <jplaggen1080@student.landstede.nl>
2021-05-25 12:58:36 +0200

    Aanpassingen Menu uren in plannen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,26 +2,42 @@
 
 @section('content')
 
+<script>
+    <?php
+        echo "const arr = ".json_encode($users).";";
+    ?>
+
+    console.log(arr);
+</script>
+
 <div class="container">
-
-
+    @if(Auth()->user()->functie == "Werkgever")
     <div id="sidemenu" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="#">About</a>
+        <a href="#" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="#" id="username"></a>
         <a href="#">Services</a>
         <a href="#">Clients</a>
         <a href="#">Contact</a>
+
     </div>
-    <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span>
+    @endif
     <script>
-        function openNav() {
-            document.getElementById("sidemenu").style.width = "250px";
+        function openNav(userid) {
+            document.getElementById("sidemenu").style.width = "400px";
+
+            for (let i =0;i<arr.length;i++) {
+                if(arr[i].id == userid) {
+                    document.getElementById('username').innerHTML = arr[i].name;
+                }
+            }
         }
 
         function closeNav() {
             document.getElementById("sidemenu").style.width = "0";
         }
     </script>
+
+
 
 
 
@@ -38,14 +54,10 @@
     ?>
 
 
-
-
-
-
     <table>
         <tr class="list">
 
-        <th class="week" ><a  href="<?php echo $_SERVER['PHP_SELF'].'?week='.($week-1).'&year='.$year; ?>"><</a>Week: <?php echo $week; ?>
+        <th class="week" ><a href="<?php echo $_SERVER['PHP_SELF'].'?week='.($week-1).'&year='.$year; ?>"><</a> Week: <?php echo $week; ?>
             <a href="<?php echo $_SERVER['PHP_SELF'].'?week='.($week+1).'&year='.$year; ?>">></a></th>
 
             <?php
@@ -57,7 +69,7 @@
         </tr>
         @foreach($users as $user)
             <tr>
-                <td class="naam">{{ $user->name }}</td>
+                <td class="naam" onclick="openNav({{ $user->id }})">{{ $user->name }} </td>
                 <td class="content">1</td>
                 <td class="content">2</td>
                 <td class="content">3</td>
